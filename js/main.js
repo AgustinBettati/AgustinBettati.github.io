b677bf87e7d0beec188c70c47695f24aba6143fe
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2018-09-18 21:56:40 -0300

    Magnetometer and gyroscope values are now displayed in a nicer way.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,63 @@
+let accelNextValue = true;
+let gyroNextValue = true;
 
-if(window.DeviceMotionEvent){
+main();
 
-    window.addEventListener("devicemotion", function (event){
-        document.getElementById("accelValues").innerHTML="Accelerometer: "
-            + event.accelerationIncludingGravity.x + ", "
-            + event.accelerationIncludingGravity.y + ", "
-            + event.accelerationIncludingGravity.z;
-    }, true);
-    console.log("DeviceMotionEvent is supported")
-
-}else{
-    document.getElementById("accelValues").innerHTML = "cannot access sensores"
+function main() {
+    if(window.DeviceMotionEvent && window.DeviceOrientationEvent){
+        addListeners();
+        console.log("DeviceMotionEvent is supported")
+    }else{
+        document.getElementById("body").innerHTML = "Cannot access sensors"
+    }
 }
 
-
-if(window.DeviceOrientationEvent){
-    window.addEventListener("deviceorientation", function orientation(event){
-        document.getElementById("magneValues").innerHTML= "Magnetometer: "
-            + event.alpha + ", "
-            + event.beta + ", "
-            + event.gamma;
-    }, false);
-}else{
-    console.log("DeviceOrientationEvent is not supported");
+function addListeners() {
+    window.addEventListener("devicemotion", accelListener, true);
+    window.addEventListener("deviceorientation", gyroListener, true);
 }
 
+function accelListener(event) {
+    if(!accelNextValue) return;
+    accelNextValue = false;
+    printAccel(event.accelerationIncludingGravity);
+    setTimeout(() => {
+        accelNextValue = true
+    }, 500);
+}
 
+function printAccel(acceleration) {
+    document.getElementById("accelValues").innerHTML =
+        "x: " + acceleration.x + "</br> "
+        + "y: " +  acceleration.y + "</br> "
+        + "z: " + acceleration.z + "</br></br>";
+}
+
+function gyroListener(event) {
+    if(!gyroNextValue) return;
+    gyroNextValue = false;
+    printGyro(event);
+    setTimeout(() => {
+        gyroNextValue = true;
+    }, 500);
+}
+
+function printGyro(gyro) {
+    document.getElementById("magneValues").innerHTML =
+        "alpha: " + gyro.alpha + "</br> "
+        + "beta: " +  gyro.beta + "</br> "
+        + "gamma: " + gyro.gamma + "</br></br>";
+}
+
+//
+// if(window.DeviceOrientationEvent){
+//     window.addEventListener("deviceorientation", function orientation(event){
+//         document.getElementById("magneValues").innerHTML= "Magnetometer: "
+//             + event.alpha + ", "
+//             + event.beta + ", "
+//             + event.gamma;
+//     }, false);
+// }else{
+//     console.log("DeviceOrientationEvent is not supported");
+// }
 
